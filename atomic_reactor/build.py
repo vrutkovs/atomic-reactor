5e44b2b6361a47d657b1faf27255add12e07ad2a
Vadim Rutkovsky <vrutkovs@redhat.com>
2016-12-09 14:29:51 +0100

    Don't look for Dockerfile

--- atomic_reactor/build.py
+++ atomic_reactor/build.py
@@ -114,11 +114,11 @@ class InsideBuilder(LastLogger, BuilderStateMachine):
         self.image = ImageName.parse(image)
 
         # get info about base image from dockerfile
-        self.df_path, self.df_dir = self.source.get_dockerfile_path()
-        self.set_base_image(df_parser(self.df_path).baseimage)
-        logger.debug("base image specified in dockerfile = '%s'", self.base_image)
-        if not self.base_image.tag:
-            self.base_image.tag = 'latest'
+        #self.df_path, self.df_dir = self.source.get_dockerfile_path()
+        #self.set_base_image(df_parser(self.df_path).baseimage)
+        #logger.debug("base image specified in dockerfile = '%s'", self.base_image)
+        #if not self.base_image.tag:
+        #    self.base_image.tag = 'latest'
 
     def build(self):
         """
